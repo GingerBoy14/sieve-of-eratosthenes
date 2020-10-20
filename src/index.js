de780b3ae7203bb34a81e63c8de62c1418b11f74
Maxim <maks.27.04.2002@gmail.com>
2020-10-20 22:15:19 +0300

    refactor main loop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const n = 20;
-const arr = Array.from({ length: n }, (_, i) => i + 2);
-for (let i = 2; i < n; i++) {
+const range = 20;
+const arr = Array.from({ length: range }, (_, i) => i + 2);
+arr.forEach((i) =>
   arr
     .filter((n) => n % i === 0)
     .slice(1)
-    .forEach((n) => arr.splice(arr.indexOf(n), 1));
-}
+    .forEach((n) => arr.splice(arr.indexOf(n), 1))
+);
 console.log(arr);
