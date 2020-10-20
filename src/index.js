45d3d5f650829bca09ebc8d7ebdf153af6dd309f
Maxim <maks.27.04.2002@gmail.com>
2020-10-20 19:13:46 +0300

    new method of init arr

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const n = 20;
-const arr = [];
-for (let i = 2; i <= n; i++) {
-  arr[i - 2] = i;
-}
+const arr = Array.from({ length: n }, (_, i) => i + 2);
 for (let i = 2; i < n; i++) {
   arr
     .filter((n) => n % i === 0)
